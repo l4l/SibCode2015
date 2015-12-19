fbd1c75510dd05d0d91c07bc7f52ad164469426c
Kitsu <mail@kitsu.me>
2015-12-19 03:52:56 +0300

    Added download/upload

--- CellManager/urls.py
+++ CellManager/urls.py
@@ -1,11 +1,14 @@
 """CellManager URL Configuration
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^upload/.*$', views.load),
-    url(r'^download/(?P<file_id>[0-9]+)/$', views.download),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^upload/$', views.load),
 ]
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- CellManager/views.py
+++ CellManager/views.py
@@ -1,4 +1,16 @@
+import random
+import string
+
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django import forms
+import os
+
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
 
 
 def miss(request):
@@ -6,13 +18,32 @@ def miss(request):
 
 
 def load(request):
-    pass
-    return HttpResponse('Hello, you\'re here')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            for key in request.FILES.keys():
+                return HttpResponseRedirect(handle_uploaded_file(request.FILES[key]))
+
+    else:
+        form = UploadFileForm()
+    return render_to_response('', {'form': form})
 
 
-def download(request, file_id):
-    i = 0
-    if not file_id.isdigit():
-        print('In:', file_id)
-        return miss(request)
-    return HttpResponse('Your request is %d.' % i)
+def handle_uploaded_file(file):
+    file_name = random_string(10)
+    while not os.path.isfile(file):
+        file_name = random_string(10)
+
+    destination = open(file_name, 'wb+')
+
+    for chunk in file.chunks():
+        destination.write(chunk)
+
+    destination.close()
+
+
+def random_string(n):
+    return ''.join(random
+                   .SystemRandom()
+                   .choice(string.ascii_uppercase + string.digits)
+                   for _ in range(n))
